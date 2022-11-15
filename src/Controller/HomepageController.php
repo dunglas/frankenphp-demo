d8295dd7400fbb6655da8100240a35b6acc6082b
Kévin Dunglas <kevin@dunglas.fr>
2022-11-15 08:14:38 +0100

    feat: add an example using 103 Early Hints

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,16 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\WebLink\Link;
 
 class HomepageController extends AbstractController
 {
     #[Route("/", name: "homepage")]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-       return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+        $response = $this->sendEarlyHints([
+            (new Link(href: '/style.css'))->withAttribute('as', 'stylesheet'),
+            (new Link(href: '/script.js'))->withAttribute('as', 'script'),
         ]);
+
+        // Do something slow...
+
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ], $response);
     }
 }
