928cc2255e436528a46c2f0dd6ff67fea3abcb7a
Kévin Dunglas <kevin@dunglas.fr>
2024-03-01 15:45:02 +0100

    chore: try to reproduce dunglas/frankenphp#617

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route("/", name: "homepage")]
-    public function index(): Response
+    public function index(Request $request): Response
     {
        return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'authorization' => $request->headers->get('Authorization'),
         ]);
     }
 }
