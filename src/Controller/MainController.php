369d991ff3d1560ab14a8da53dc75e6988a20a34
Kévin Dunglas <kevin@dunglas.fr>
2024-07-02 23:15:48 +0200

    feat: X-Sendfile example

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route("/", name: "homepage")]
     public function index(): Response
@@ -14,5 +15,14 @@ class HomepageController extends AbstractController
        return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
+    }
+
+    #[Route("/download-logo", name: "download_logo")]
+    public function downloadLogo(): BinaryFileResponse
+    {
+        $response = new BinaryFileResponse(__DIR__.'/../../private-files/frankenphp.png');
+        $response->headers->set('Content-Type', 'image/png');
+
+        return $response;
     }
 }
