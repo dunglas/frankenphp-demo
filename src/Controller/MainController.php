4c445d5e39c6e867a23e0cf370af6735bf4605fa
Christopher Georg <christopher.georg@sr-travel.de>
2025-12-30 17:16:22 +0100

    feat: fix deprecations

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
